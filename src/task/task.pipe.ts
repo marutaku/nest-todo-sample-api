5ed608093416dc453814e75fada3fb5fbb2dc3b9
Takumi Maruyama <satouredstone@gmail.com>
2022-03-02 14:17:45 +0000

    lint対応

--- src/task/task.pipe.ts
+++ src/task/task.pipe.ts
@@ -1,14 +1,9 @@
-import {
-  ArgumentMetadata,
-  BadGatewayException,
-  Injectable,
-  PipeTransform,
-} from '@nestjs/common';
+import { BadGatewayException, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class TaskStatusPipe implements PipeTransform {
   readonly ALLOWED_STATUS = ['OPEN', 'PROGRESS', 'DONE'];
-  transform(value: string, metadata: ArgumentMetadata) {
+  transform(value: string) {
     value = value.toUpperCase();
     if (!this.ALLOWED_STATUS.includes(value)) {
       throw new BadGatewayException(`invalid status: ${value}`);

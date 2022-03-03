74544ce8229a97a7027ae021dc9144849680082d
Takumi Maruyama <satouredstone@gmail.com>
2022-03-03 16:18:06 +0000

    いらないインポートを削除

--- config/typeorm-config.service.ts
+++ config/typeorm-config.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { config } from 'process';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {

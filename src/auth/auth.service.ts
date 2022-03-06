66a992b17d5f07b425293eacf26b6bfc52a16932
Takumi Maruyama <satouredstone@gmail.com>
2022-03-06 10:40:40 +0000

    feat: authモジュールを追加

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+  async validateUser(username: string, password: string) {
+    const user = await this.userService.findByName(username);
+    if (user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}

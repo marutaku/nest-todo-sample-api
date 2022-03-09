4c9caf23b99ef3db9ca7f260e0645a1ab8d82bd6
Takumi Maruyama <satouredstone@gmail.com>
2022-03-09 14:11:09 +0000

    feat: マイグレーションを作成

---
+++ migrations/1646835023794-CreateUserTable.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUserTable1646835023794 implements MigrationInterface {
+    name = 'CreateUserTable1646835023794'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "password" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}

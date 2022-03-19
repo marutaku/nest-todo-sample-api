f499efdbf45095cb60d2a30e847395fae7f321c4
Takumi Maruyama <satouredstone@gmail.com>
2022-03-19 13:00:59 +0000

    feat: /projectにもミドルウェアを使用する

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '../users/users.module';
 import { Project } from './project.entity';
 import { ProjectsController } from './projects.controller';
+import { ProjectsMiddleware } from './projects.middleware';
 import { ProjectsService } from './projects.service';
 
 @Module({
@@ -11,4 +17,11 @@ import { ProjectsService } from './projects.service';
   controllers: [ProjectsController],
   exports: [ProjectsService],
 })
-export class ProjectsModule {}
+export class ProjectsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ProjectsMiddleware).forRoutes({
+      path: '/projects/*+',
+      method: RequestMethod.ALL,
+    });
+  }
+}

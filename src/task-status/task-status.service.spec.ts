9a399e3b86ec61ee33fc29701f5808c1ab8d6275
Takumi Maruyama <satouredstone@gmail.com>
2022-03-21 14:56:14 +0000

    test: テストを修正

--- src/task-status/task-status.service.spec.ts
+++ src/task-status/task-status.service.spec.ts
@@ -1,18 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { useMockRepositoryProvider } from '../share/test-support';
+import { TaskStatus } from './task-status.entity';
 import { TaskStatusService } from './task-status.service';
 
 describe('TaskStatusService', () => {
   let service: TaskStatusService;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [TaskStatusService],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TaskStatusService, useMockRepositoryProvider(TaskStatus)],
+    }).compile();
 
-  //   service = module.get<TaskStatusService>(TaskStatusService);
-  // });
+    service = module.get<TaskStatusService>(TaskStatusService);
+  });
 
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
 });

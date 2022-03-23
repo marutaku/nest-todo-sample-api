bf1538f3209302ad0a6c6015368c2f476bb71e81
Takumi Maruyama <satouredstone@gmail.com>
2022-03-23 14:16:54 +0000

    fix: モジュールを追加

--- src/task-status/task-status.module.ts
+++ src/task-status/task-status.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardsModule } from '../boards/boards.module';
 import { TaskStatus } from './task-status.entity';
 import { TaskStatusService } from './task-status.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskStatus])],
+  imports: [TypeOrmModule.forFeature([TaskStatus]), BoardsModule],
   providers: [TaskStatusService],
 })
 export class TaskStatusModule {}

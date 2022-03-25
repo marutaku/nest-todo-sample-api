e2d24ecb93bf2790dad8bac8ef666c7e60fb1f7f
Takumi Maruyama <satouredstone@gmail.com>
2022-03-25 12:18:33 +0000

    feat: アップデート用のdtoを用意

--- src/task-status/task-status.dto.ts
+++ src/task-status/task-status.dto.ts
@@ -1,9 +1,17 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Min } from 'class-validator';
 
 export class TaskStatusDto {
   @IsNotEmpty()
   name: string;
 
+  @Min(1)
   @IsNotEmpty()
   order: number;
+}
+
+export class TaskStatusUpdateDto {
+  name?: string;
+
+  @Min(1)
+  order?: number;
 }

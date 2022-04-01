09cd22d5f9096b4901f58b7c77cd6a38dc8cfda6
Takumi Maruyama <satouredstone@gmail.com>
2022-04-01 14:13:39 +0000

    feat: graphQL用のフィールドを追加

--- src/projects/project.entity.ts
+++ src/projects/project.entity.ts
@@ -1,3 +1,4 @@
+import { Field, ObjectType } from '@nestjs/graphql';
 import {
   Column,
   CreateDateColumn,
@@ -12,21 +13,27 @@ import { Board } from '../boards/board.entity';
 import { User } from '../users/user.entity';
 
 @Entity()
+@ObjectType()
 export class Project {
   @PrimaryGeneratedColumn('uuid')
+  @Field()
   id: string;
 
   @Column()
+  @Field()
   name: string;
 
   @Column()
+  @Field()
   description: string;
 
   @ManyToMany(() => User, (user) => user.projects)
   @JoinTable()
+  @Field((type) => [User])
   users: User[];
 
   @OneToMany(() => Board, (board) => board.project)
+  @Field((type) => [Board])
   boards: Board[];
 
   @CreateDateColumn()

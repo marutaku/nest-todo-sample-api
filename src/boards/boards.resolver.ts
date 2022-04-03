cf3d489690f718f3b7ff9cf5b241c276923149a3
Takumi Maruyama <satouredstone@gmail.com>
2022-04-03 02:34:27 +0000

    feat: クエリを追加

--- src/boards/boards.resolver.ts
+++ src/boards/boards.resolver.ts
@@ -1,5 +1,12 @@
-import { Inject } from '@nestjs/common';
-import { Args, Resolver, Query, ResolveField, Parent } from '@nestjs/graphql';
+import { Inject, ParseIntPipe, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Args,
+  Resolver,
+  Query,
+  ResolveField,
+  Parent,
+  Int,
+} from '@nestjs/graphql';
 import { TaskStatus } from '../task-status/task-status.entity';
 import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
@@ -8,9 +15,16 @@ import { BoardsService } from './boards.service';
 export class BoardsResolver {
   constructor(@Inject(BoardsService) private boardsService: BoardsService) {}
 
+  @Query(() => Board)
+  async fetchBoardById(
+    @Args('boardId', { type: () => Int }, ParseIntPipe) boardId: number,
+  ) {
+    return this.boardsService.getBoardById(boardId);
+  }
+
   @Query(() => [Board])
   async fetchBoardsByProjectId(
-    @Args('projectId', { type: () => String }) projectId: string,
+    @Args('projectId', { type: () => String }, ParseUUIDPipe) projectId: string,
   ) {
     return this.boardsService.getBoards(projectId);
   }
